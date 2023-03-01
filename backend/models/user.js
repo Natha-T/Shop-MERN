11e2de492d03c01286feeb7c51447f5c10242760
SNPXX <76396243+Natha-T@users.noreply.github.com>
2023-03-01 17:25:37 +0100

    buildfix

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -34,4 +34,5 @@ const userSchema = new Schema({
 });
 
  const User = mongoose.model('User', userSchema);
+
 exports.User = User;
