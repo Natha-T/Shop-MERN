2bb9da93b0b72b2055b7080ad390aa3a776a207a
SNPXX <76396243+Natha-T@users.noreply.github.com>
2023-03-01 16:41:43 +0100

    build

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ require("dotenv").config()
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://foodappfront.onrender.com/',
     credentials: true,
  }));
 
